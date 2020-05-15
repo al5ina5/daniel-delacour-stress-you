ab9e4c1036ca84afb460877db3a9c05b5eb7c8a1
Sebastian Alsina <alsinas@me.com>
2020-05-15 00:13:24 -0400

    Changed OG-IMAGE Size

--- src/components/Meta.jsx
+++ src/components/Meta.jsx
@@ -10,7 +10,7 @@ const Meta = () => (
         <meta name="og:description" property="og:description" content="Stress You by Daniel Delacour is out now. Streaming everywhere." />
         <meta property="og:site_name" content="Stress You" />
         <meta property="og:url" content="https://danieldelacour.com" />
-        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:card" content="summary_large" />
         <meta name="twitter:title" content="Stress You" />
         <meta name="twitter:description" content="Now streaming everywhere," />
         <meta name="twitter:site" content="https://danieldelacour.com" />
@@ -18,7 +18,7 @@ const Meta = () => (
         <link rel="icon" type="image/png" href="/img/favicon.ico" />
         <link rel="apple-touch-icon" href="/img/favicon.png" />
         <meta property="og:image" content="/img/og-image.jpg" />
-        <meta name="twitter:image" content="https://daniel-delacour-stress-you.now.sh/img/og-image.jpg" />
+        <meta name="twitter:image" content="https://daniel-delacour-stress-you.now.sh/img/og-image-square.jpg" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" />
     </Head>
 )
