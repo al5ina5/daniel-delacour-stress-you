cc6cec485945dbadce7e88405246e3dd68eb041a
Sebastian Alsina <alsinas@me.com>
2020-05-14 22:28:12 -0400

    Added Heart Motion

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,29 +1,40 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Particles from 'react-particles-js'
-import Tilt from 'react-tilt'
+import { motion } from 'framer-motion'
 
 import styles from './index.module.scss'
 import ParticlesConfig from './particles.json'
 
 class Index extends React.Component {
-    state = {
+    constructor(props) {
+        super(props)
 
+        this.state = {}
     }
 
     render() {
         return (
-            <Tilt className={styles.landing} options={{ max: 25, perspective: 2000 }}>
+            <div ref={this.constraintsRef} className={styles.landing}>
                 <Particles className={styles.particles} params={ParticlesConfig} />
                 <div className={styles.notice}>[Click Anywhere]</div>
-                <img src="/img/stress-you-heart.png" alt="" className={styles.heart} />
-
+                <motion.div
+                    drag={true}
+                    initial={{ rotate: 180, scale: 0 }}
+                    animate={{ rotate: 0, scale: 1 }}
+                    transition={{
+                        type: "spring",
+                        stiffness: 100,
+                        damping: 100
+                    }}>
+                    <img src="/img/stress-you-heart.png" alt="" className={styles.heart} />
+                </motion.div>
                 <div className={styles.socials}>
                     <a href="# " target="_blank" rel="noopener noreferrer"><i className="fab fa-apple"></i></a>
                     <a href="# " target="_blank" rel="noopener noreferrer"><i className="fab fa-spotify"></i></a>
                     <a href="# " target="_blank" rel="noopener noreferrer"><i className="fab fa-soundcloud"></i></a>
                     <a href="# " target="_blank" ><i className="fab fa-youtube"></i></a>
                 </div>
-            </Tilt>
+            </div>
         )
     }
 }
